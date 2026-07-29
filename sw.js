44aef3d46aba40e56c278b3b6374e36b027a9c0d
阅见超凡 <945656286@qq.com>
2026-07-29 16:12:26 +0800

    Update sw.js: AI daily rotation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gw-v5';
+const CACHE='gw-v6';
 const CORE=['index.html','icon.svg','icon-192.png','icon-512.png','manifest.webmanifest'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
